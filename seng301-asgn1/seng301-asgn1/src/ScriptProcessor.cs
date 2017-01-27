ba8266a030cd8aa49538b9f17f89673a66343214
Tony Tang <tonyt@ucalgary.ca>
2017-01-27 14:12:14 -0700

    FIX: ScriptProcessor.cs Update to facilitate grading

--- seng301-asgn1/seng301-asgn1/src/ScriptProcessor.cs
+++ seng301-asgn1/seng301-asgn1/src/ScriptProcessor.cs
@@ -4,43 +4,62 @@ using System.IO;
 using Frontend1;
 using Frontend1.Parser;
 
-namespace seng301_asgn1 {
-    public class ScriptProcessor {
-        VendingMachineParser parser;
-        SENG301VMAnalyzer analyzer;
+public class ScriptProcessor {
+    VendingMachineParser parser;
+    SENG301VMAnalyzer analyzer;
 
-        public ScriptProcessor(TextReader reader, IVendingMachineFactory factory) {
-            this.analyzer = new SENG301VMAnalyzer();
-            this.analyzer.RegisterVendingMachineFactory(factory);
-            this.parser = new VendingMachineParser(reader, this.analyzer);
-        }
+    public ScriptProcessor(TextReader reader, IVendingMachineFactory factory) {
+        this.analyzer = new SENG301VMAnalyzer();
+        this.analyzer.RegisterVendingMachineFactory(factory);
+        this.parser = new VendingMachineParser(reader, this.analyzer);
+    }
 
-        public ScriptProcessor(string pathToScript, IVendingMachineFactory factory) :
-            this(new StreamReader(File.OpenRead(pathToScript)), factory) {
-        }
+    public ScriptProcessor(string pathToScript, IVendingMachineFactory factory) :
+        this(new StreamReader(File.OpenRead(pathToScript)), factory) {
+    }
 
-        public void Parse() {
-            this.parser.Parse();
-        }
-
-        public static void Main(string[] args) {
-            var goodScripts = Directory.GetFiles("test-scripts", "T*");
-            foreach (var script in goodScripts) {
-                Console.WriteLine(script + ":");
-                var scriptParser = new ScriptProcessor(script, new VendingMachineFactory());
+    public void Parse() {
+        this.parser.Parse();
+    }
+    
+    public static void Main(string[] args) {
+        int totalTests = 0;
+        int passedTests = 0;
+        var goodScripts = Directory.GetFiles("test-scripts", "T*");
+        foreach(var script in goodScripts) {
+            var pass = true;
+            Console.Write(script + ":");
+            try {
+                var scriptParser = new ScriptProcessor(new StreamReader(File.OpenRead(script)), new VendingMachineFactory());
                 scriptParser.Parse();
             }
-            var badScripts = Directory.GetFiles("test-scripts", "U*");
-            foreach (var script in badScripts) {
-                Console.WriteLine(script + ":");
-                try {
-                    var scriptParser = new ScriptProcessor(script, new VendingMachineFactory());
-                    scriptParser.Parse();
-                }
-                catch {
-                    Console.WriteLine("..FAIL (which is good)");
-                }
+            catch {
+                pass = false;
             }
+            Console.WriteLine(pass ? " PASS=Good" : " FAIL=Bad");
+            if (pass) {
+                passedTests++;
+            }
+            totalTests++;
         }
+        var badScripts = Directory.GetFiles("test-scripts", "U*");
+        foreach(var script in badScripts) {
+            var pass = true;
+            Console.Write(script + ":");
+            try {
+                var scriptParser = new ScriptProcessor(new StreamReader(File.OpenRead(script)), new VendingMachineFactory());
+                scriptParser.Parse();
+            }
+            catch {
+                pass = false;
+            }
+            Console.WriteLine(pass ? " PASS=Bad" : " FAIL=Good");
+            if (!pass) {
+                passedTests++;
+            }
+            totalTests++;
+        }
+
+        Console.WriteLine("{0}/{1} tests passed", passedTests, totalTests);
     }
 }
